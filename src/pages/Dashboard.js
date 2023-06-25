8f10e9abfafcabd0e4f4fe9af101cb92d6996f12
pallavi <pallavimahanta09@gmail.com>
2023-06-25 14:42:03 +0530

    first commit

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,17 @@
+
+  
+
+ import React from 'react'
+ 
+ const Dashboard = () => {
+   return (
+     <div  className=" w-screen h-screen flex justify-center items-center text-white text-3xl  bg-black" >
+       Dashboard!
+       
+     </div>
+   )
+ }
+ 
+ export default Dashboard
+ 
+ 
